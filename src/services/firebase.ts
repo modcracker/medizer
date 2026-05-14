8990705f9b47781f732f726c92596641b9f0009e
Evu Admin <modcracker@users.noreply.github.com>
2026-05-14 14:19:46 -0400

    Add files via upload

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+import firebaseConfig from '@/firebase-applet-config.json';
 
 let app;
 try {
